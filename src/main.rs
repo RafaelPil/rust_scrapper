0db53d0a8aef4d912cae293d4fe50a00e31a6837
RafaelPil <rafik.pilumyan@gmail.com>
2025-04-16 22:22:12 +0300

    Update: fn parse_html

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,26 @@ async fn fetch_html(url: String) -> Result<String, Error> {
     let response = reqwest::get(&url).await?;
     response.text().await
 }
+
+fn parse_html(html: &str) -> ParseData {
+    let document = Html::parse_document(html);
+    let title_selector = Selector::parse("h1, h2, h3").unwrap();
+    let links_selector = Selector::parse("a").unwrap();
+
+    let titles = document
+        .select(&title_selector)
+        .map(|element| element.text().collect::<Vec<_>>().join(""))
+        .collect();
+
+    let links = document
+        .select(&links_selector)
+        .filter_map(|element| element.value().attr("href"))
+        .map(String::from)
+        .collect();
+
+    ParseData {titles, links}
+}
+
 fn main() {
     println!("Hello, world!");
 }
